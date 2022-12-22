7f39caa8cc1181e5c706c2ca56649514b16b2b0a
Valepa2911 <megp17102017@mail.ru>
2022-12-22 21:23:50 +0400

    сохран.

--- Zadacha/Program.cs
+++ Zadacha/Program.cs
@@ -1,3 +1,21 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 
+Console.WriteLine("строка массива");
+int mas = Convert.ToInt32 (Console.ReadLine());
+string[] array = new string[mas];
+string[] ar2 = new string[3];
+
+for (int i = 0; i < stringArray.length; i++)
+{
+    
+}
+
+
+
+
+
+
+
+
+//Console.WriteLine(array);
