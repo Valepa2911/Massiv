d922a0b0aa9527f488d43a939b7e9b29931e3512
Valepa2911 <megp17102017@mail.ru>
2022-12-25 23:46:10 +0400

    сохран.2

--- Zadacha/Program.cs
+++ Zadacha/Program.cs
@@ -2,18 +2,20 @@
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 
 Console.WriteLine("строка массива");
-int mas = Convert.ToInt32 (Console.ReadLine());
+int mas;
+int.TryParse(Console.ReadLine(), out mas);
 string[] array = new string[mas];
 string[] ar2 = new string[array.Length];
-int count = 0;
+
 for (int i = 0; i < array.Length; i++)
 {
-    if (array[i].Length<=0)
+    int count = 0;
+    if (array[i].Length <= 3)
     {
-          ar2[count] = array[i];
-          count++;
+        ar2[count] = array[i];
+        count++;
     }
-    Console.WriteLine(ar2[count]);
+    Console.WriteLine(string.Join(",",ar2[count]));
 }
 
 
