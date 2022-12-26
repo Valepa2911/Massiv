c5d04a5e37af679738c8ad8d0376aaed3f634d6d
Valepa2911 <megp17102017@mail.ru>
2022-12-26 20:08:12 +0400

    сохран.4

--- Zadacha/Program.cs
+++ Zadacha/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 
-string[] array = { "14", "hello", "d=)", "2", "777", "dsaFD", "258963", "World" };
+string[] array = { "1k", "hello", "d=)", "2", "777", "dsaFD", "258963", "World" };
 string[] ar2 = new string[array.Length];
 int b = 3;
 Vivod(array);
@@ -9,26 +9,19 @@ Vivod(array);
 void Vivod(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
-    {
+
         Console.Write(" " + array[i]);
-    }
+
     Console.WriteLine();
 }
 
-
 for (int i = 0; i < array.Length; i++)
 {
-
-
     if (array[i].Length <= b)
-    {
         ar2[i] = array[i];
-
-    }
     else
         ar2[i] = String.Empty;
     Console.Write(" " + ar2[i]);
-
 }
 
 
